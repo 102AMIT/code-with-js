753ad143fdfedaadc71ded97d466184a3e3b1971
Amit Kumar Thakur <kingston.amit9830@gmail.com>
2023-01-27 00:07:48 +0530

    first 3 char

---
+++ first3Char.js
@@ -0,0 +1,7 @@
+// Write a function that takes a string (a) as argument. Get the first 3 characters of a. Return the result
+
+function firstThree(str) {
+    return str.slice(0, 3);
+}
+
+console.log(firstThree("abafgfhja"));
