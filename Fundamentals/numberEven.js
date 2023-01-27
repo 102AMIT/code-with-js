3e0276a635d08140e524d169dcc289d8a3295961
Amit Kumar Thakur <kingston.amit9830@gmail.com>
2023-01-27 11:34:11 +0530

    even number

---
+++ Fundamentals/numberEven.js
@@ -0,0 +1,10 @@
+// check the number is even or not return true or false
+
+function isEven(num) {
+    return num % 2 === 0;
+}
+
+
+console.log(isEven(10));
+console.log(isEven(15));
+console.log(isEven(-4));
